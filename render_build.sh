60fc8701b01c2bf91f524a8b93b557913c903553
Jose Luis Vela <jlvela94@gmail.com>
2025-10-05 16:21:39 +0000

    change render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-npm install
+#!/bin/bash
+# Frontend
+npm ci
 npm run build
 
+# Backend
+pip install --upgrade pip
 pip install -r requirements.txt
-
-pip run upgrade
+# No flask run aquí; migraciones opcionales si son necesarias
+# flask db upgrade  # Mejor en Start Command
